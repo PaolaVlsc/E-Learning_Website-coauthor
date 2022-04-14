3d77dea2457c457e46685e2133eddbbdfe3f0cf2
ChaniotakisNikolaos <nxnick26@gmail.com>
2022-04-14 14:08:14 +0300

    Προσθήκη εντολών δημιουργίας βάσης και πινάκων

--- projectEL.sql
+++ projectEL.sql
@@ -2,4 +2,7 @@
 DROP DATABASE IF EXISTS projectELDb;
 CREATE DATABASE projectELDb;
 USE projectELDb;
--- Dummny Data insertion commands
+CREATE TABLE users ( id INT NOT NULL, name VARCHAR(25) NOT NULL, password VARCHAR(32) NOT NULL, email VARCHAR(100) NOT NULL, average FLOAT, PRIMARY KEY (id) );
+CREATE TABLE chapter ( id INT NOT NULL, name VARCHAR(100), PRIMARY KEY (id) );
+CREATE TABLE grades ( id INT NOT NULL AUTO_INCREMENT, chapter_id INT, user_id INT, grade INT, PRIMARY KEY(id), FOREIGN KEY(chapter_id) REFERENCES chapter(id), FOREIGN KEY(user_id) REFERENCES users(id) );
+-- Dummy Data insertion commands
