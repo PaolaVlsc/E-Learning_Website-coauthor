bfec3a112bbdd68755e0e150f7db9d708a958cc2
ChaniotakisNikolaos <nxnick26@gmail.com>
2022-04-14 16:17:04 +0300

    Λήψη σύνδεσης με βάση

--- src/main/java/projectel/projectel/HelloServlet.java
+++ src/main/java/projectel/projectel/HelloServlet.java
@@ -1,26 +1,23 @@
 package projectel.projectel;
 
-import java.io.*;
-import java.sql.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
-        try {
-            Class.forName("org.mariadb.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/bookstore_sinp", "root", "");
-            message=conn.toString();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        message = "Connection name=";
+        Connection conn= DbConnection.getConnection();
+        if (conn!=null)
+            message+=conn;
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
