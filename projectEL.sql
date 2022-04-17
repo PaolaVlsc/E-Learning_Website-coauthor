38feb738627332b14d11c7d61d13ea582d49ccd4
LimnosJason <101047420+LimnosJason@users.noreply.github.com>
2022-04-17 15:31:52 +0300

    Update projectEL.sql

--- projectEL.sql
+++ projectEL.sql
@@ -19,3 +19,16 @@ END;
 //
 delimiter ;
 -- Dummy Data insertion commands
+
+-- Instert Dummy Data for users table
+INSERT INTO users VALUES (0,'Βεμπληχα','Βεμπληχα321','Beblixa@gmail.com',NULL),(1,'Πάολα','BestEngineer321','Paola@gmail.com',NULL),(2,'Νίκος','ILoveFortNite','Nikos@gmail.com',NULL),(3,'Σοφία','HandCleanerGel','Sofia@gmail.com',NULL),(4,'Ιάσονας','Iasonas123','Iasonas@gmail.com',NULL),(5,'Παναγιώτης','TankDriver7','Panagioths@gmail.com',NULL),(6,'Μαίρη','ILoveBTS','Mairh@gmail.com',NULL),(7,'Μάρκος','HarryPoter21','Petros@gmail.com',NULL),(8,'Αθηνά','1234ac','Athina@gmail.com',NULL),(9,'Γιάννης','JohnPro','John@gmail.com',NULL),(10,'Κίκη','JustinBieber','Kikh@gmail.com',NULL);
+
+-- Instert Dummy Data for chapter table
+INSERT INTO chapter VALUES (1,'Κεφαλαιο 1'),(2,'Κεφαλαιο 2'),(3,'Κεφαλαιο 3'),(4,'Κεφαλαιο 4'),(5,'Τελικό Τεστ');
+
+-- Instert Dummy Data for grades table
+INSERT INTO grades (chapter_id,user_id,grade) VALUES (1,1,7),(1,2,5),(2,2,6),(3,2,7),(4,2,8),(1,3,2),(2,3,2),(3,3,7),(4,3,4),(5,3,5);
+
+INSERT INTO grades (chapter_id,user_id,grade) VALUES (1,4,7),(1,4,5),(1,4,6),(1,5,0),(1,5,8),(1,5,2),(2,5,10),(2,5,10),(3,5,4),(3,5,5),(4,5,1),(4,5,9),(4,5,8), (1,6,4),(1,6,8),(1,6,4),(2,6,10),(3,6,2),(3,6,8),(4,6,1),(4,6,9),(4,6,8),(5,6,2),(5,6,3),(4,6,9);
+
+INSERT INTO grades (chapter_id,user_id,grade) VALUES (3,7,10),(4,7,10),(1,8,1),(1,8,7),(1,8,8),(1,8,10),(2,8,10),(1,9,7),(2,9,9),(5,9,7),(2,10,7),(2,10,10),(3,10,2),(3,10,5),(3,10,7),(5,10,2),(5,10,6),(5,10,8),(5,10,10);
