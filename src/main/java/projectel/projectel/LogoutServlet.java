f5211bf63c5ddfc24aed656a60f1c42ede7d10af
ChaniotakisNikolaos <nxnick26@gmail.com>
2022-04-20 16:44:50 +0300

    UPDATED LogoutServlet.java

--- src/main/java/projectel/projectel/LogoutServlet.java
+++ src/main/java/projectel/projectel/LogoutServlet.java
@@ -11,17 +11,6 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "logoutServlet", value = "/logout-servlet")
 public class LogoutServlet extends HttpServlet {
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
-        response.setContentType("text/html;charset=UTF-8");
-        HttpSession session = request.getSession();
-        session.removeAttribute("userId");
-        session.removeAttribute("userCategory");
-        try {
-            response.sendRedirect("jsp/index.jsp");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
