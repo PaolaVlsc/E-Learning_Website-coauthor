1f5b684423b7c362ee6a5dd0b9eb90b82df24176
LimnosJason <limnosjason@gmail.com>
2022-05-01 20:04:34 +0300

    Revert "chapter parameter"
    This reverts commit e157157dced5c5c40eec83fd0fbff08668877bc5.

--- src/main/webapp/js/quiz.js
+++ src/main/webapp/js/quiz.js
@@ -466,12 +466,6 @@
             }
         }
     ];
-
-    //get url
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const chapter = urlParams.get('chapter')
-
     let numCorrect = -1;
     let chosenQuestions=[];
     //Shuffle quiz questions
