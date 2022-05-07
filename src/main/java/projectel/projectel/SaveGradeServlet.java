df03f49535e3061f88ba8fe77cadfe6e3b0ce571
ChaniotakisNikolaos <nxnick26@gmail.com>
2022-05-07 18:48:30 +0300

    save grade

---
+++ src/main/java/projectel/projectel/SaveGradeServlet.java
@@ -0,0 +1,18 @@
+package projectel.projectel;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SaveGradeServlet", value = "/SaveGradeServlet")
+public class SaveGradeServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       request.getParameter("grade");
+       System.out.println("grade="+request.getParameter("grade"));
+    }
+
+}
